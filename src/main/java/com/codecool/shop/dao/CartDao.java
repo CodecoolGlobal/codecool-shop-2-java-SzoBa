01a52d175e43f72182bd1b776330096db4107382
Márton Kozma <kozmartinus@gmail.com>
2020-10-13 19:58:48 +0200

    create CartDao interface

---
+++ src/main/java/com/codecool/shop/dao/CartDao.java
@@ -0,0 +1,15 @@
+package com.codecool.shop.dao;
+
+import com.codecool.shop.model.Cart;
+
+import java.util.List;
+
+public interface CartDao {
+
+    void add(Cart cart);
+    void add(Cart cart, int clientSessionHashCode);
+    Cart find(int id);
+    void remove(int id);
+
+    List<Cart> getAll();
+}
