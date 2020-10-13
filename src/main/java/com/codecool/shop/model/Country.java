e33fc151d0249255e384dbaa5c9989aaa6a79d8a
Balázs Szolcsánszki <bszolcsan@gmail.com>
2020-10-13 10:26:14 +0200

    Change model names

---
+++ src/main/java/com/codecool/shop/model/Country.java
@@ -0,0 +1,47 @@
+package com.codecool.shop.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Country extends BaseModel {
+
+    private String name;
+    private List<MatchDetails> matchDetails;
+
+    public Country(String name, String description) {
+        super(description);
+        this.name = name;
+        this.matchDetails = new ArrayList<>();
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setMatchDetails(ArrayList<MatchDetails> matchDetails) {
+        this.matchDetails = matchDetails;
+    }
+
+    public List<MatchDetails> getMatchDetails() {
+        return this.matchDetails;
+    }
+
+    public void addMatchDetails(MatchDetails matchDetails) {
+        this.matchDetails.add(matchDetails);
+    }
+
+//    @Override
+//    public String toString() {
+//        return String.format(
+//                "id: %1$d," +
+//                        "name: %2$s, " +
+//                        "description: %4$s",
+//                this.id,
+//                this.name,
+//                this.description);
+//    }
+}
