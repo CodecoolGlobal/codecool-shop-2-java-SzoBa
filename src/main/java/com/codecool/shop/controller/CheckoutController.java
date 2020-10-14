6d75be4eb92766d50cc7ff8d215e0c8f5407bac5
Márton Kozma <kozmartinus@gmail.com>
2020-10-14 15:34:47 +0200

    add redirect to home page if cart is null

--- src/main/java/com/codecool/shop/controller/CheckoutController.java
+++ src/main/java/com/codecool/shop/controller/CheckoutController.java
@@ -1,15 +1,19 @@
 package com.codecool.shop.controller;
 
 import com.codecool.shop.config.TemplateEngineUtil;
+import com.codecool.shop.dao.CartDao;
 import com.codecool.shop.dao.CountryDao;
 import com.codecool.shop.dao.MatchDetailsDao;
 import com.codecool.shop.dao.SportTypeDao;
+import com.codecool.shop.dao.implementation.CartDaoMem;
 import com.codecool.shop.dao.implementation.CountryDaoMem;
 import com.codecool.shop.dao.implementation.MatchDetailsDaoMem;
 import com.codecool.shop.dao.implementation.SportTypeDaoMem;
+import com.codecool.shop.model.Cart;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -22,10 +26,22 @@ public class CheckoutController extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
-        WebContext context = new WebContext(req, resp, req.getServletContext());
+        CartDao cartDao = CartDaoMem.getInstance();
+        int clientSessionIdHashCode = req.getSession().getId().hashCode();
+        Cart cart = cartDao.find(clientSessionIdHashCode);
 
-        resp.setContentType("text/html; charset=UTF-8");
-        engine.process("checkout.html", context, resp.getWriter());
+        if (cart == null) {
+            resp.sendRedirect(req.getContextPath() + "/");
+        } else {
+            TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
+            WebContext context = new WebContext(req, resp, req.getServletContext());
+
+            context.setVariable("cart", cart);
+
+
+            resp.setContentType("text/html; charset=UTF-8");
+            engine.process("checkout.html", context, resp.getWriter());
+        }
+
     }
 }
