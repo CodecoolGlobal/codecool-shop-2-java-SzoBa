47e63ec1d0f87d0f2876b9e627154a472e3dd36e
Balázs Szolcsánszki <bszolcsan@gmail.com>
2020-10-14 23:15:35 +0200

    Add content js for html elements

--- src/main/webapp/static/js/dom.js
+++ src/main/webapp/static/js/dom.js
@@ -1,4 +1,5 @@
 import {dataHandler} from "./dataHandler.js";
+import {contentItems} from "./contentItems.js";
 
 export let dom = {
     init: function () {
@@ -101,47 +102,14 @@ export let dom = {
 
     clearCartContentBody: function () {
         let content = document.querySelector(".cart-content-body");
-        content.innerHTML =
-            `<div class="cart-content-matches">
-            <ul></ul>
-            </div>`;
+        content.innerHTML = contentItems.clearCartContentBody();
+
     },
 
     createMatchesOnMainTable: function (matches) {
         let mainContent = "";
         for (let match of matches) {
-            let matchContent = `
-            <div class="col col-sm-12 col-md-6 col-lg-4">
-                <div class="card" data-matchId="${match.id}">
-                    <div class="card-header">
-                        <h4 class="card-title">${match.homeTeam} - ${match.awayTeam}</h4>
-                        <p class="card-text">${match.leagueName}</p>
-                    </div>
-                    <div class="card-body">
-                        <div class="card-odds" data-outcome="home">
-                            <div class="btn btn-success">
-                                <span>H</span>
-                                <br>
-                                    <span>${match.homeOdds % 1 === 0 ? match.homeOdds + '.0' : match.homeOdds}</span>
-                            </div>
-                        </div>
-                        <div class="card-odds ${match.drawOdds === 1 ? "hidden" : ""}" data-outcome="draw">
-                            <div class="btn btn-success">
-                                <span>D</span>
-                                <br>
-                                    <span>${match.drawOdds % 1 === 0 ? match.drawOdds + '.0' : match.drawOdds}</span>
-                            </div>
-                        </div>
-                        <div class="card-odds" data-outcome="away">
-                            <div class="btn btn-success">
-                                <span>V</span>
-                                <br>
-                                    <span>${match.awayOdds % 1 === 0 ? match.awayOdds + '.0' : match.awayOdds}</span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>`
+            let matchContent = contentItems.mainTableMatch(match);
             mainContent += matchContent;
         }
         document.getElementById("matches").insertAdjacentHTML("beforeend", mainContent);
@@ -180,15 +148,7 @@ export let dom = {
         let matchList = document.querySelector(".cart-content-matches ul");
         let content = "";
         for (let match of matches) {
-            let addMatch = `
-            <li class ="cart-item" data-matchid="${match.matchId}">
-                <div>${match.home} - ${match.away}</div>
-                <div>
-                    Chosen: ${match.chosenOutcome}
-                    <a><img src="/static/img/trashbin3.png" width="15" height="15" alt="delete-match"></a>
-                </div>
-                <span class="odds">Odds: <span>${match.odds}</span></span>
-            </li>`
+            let addMatch = contentItems.cartMatch(match);
             content += addMatch;
         }
 
@@ -198,36 +158,14 @@ export let dom = {
     addFancyTextToEmptyCart: function () {
         let matchList = document.querySelector(".cart-content-matches ul");
         let matchContent = document.querySelector(".cart-content-matches");
-        let addNoMatches = `<div class="no-matches">
-                No matches added to cart yet. </br>
-                Please choose to continue!
-            </div>`
+        let addNoMatches = contentItems.emptyCartFancyText();
         matchList.insertAdjacentHTML("beforeend", addNoMatches);
         matchContent.classList.add("align-no-matches")
     },
 
     addCartConstantItems: function (data) {
         let content = document.querySelector(".cart-content-body");
-        let items =
-            `<div class="cart-inputs">
-                 <div>
-                     <label class="bet-value-label" for="betValue">Current bet: </label>
-                     <input class="bet-value-input" id="betValue" name="betValue" type="text" placeholder="Min 100">
-                 </div>
-                 <table class="cart-input-table">
-                     <tr>
-                         <td>Total odds: </td>
-                         <td id="total-odds-value">${data.odds}</td>
-                     </tr>
-                     <tr>
-                         <td>Possible win: </td>
-                         <td id="possible-win-value">0</td>
-                     </tr>
-                 </table>
-             </div>
-             <div class="confirmation">
-                <a href="/"><button class="go-to-checkout">Confirm Order</button></a>
-             </div>`
+        let items = contentItems.cartConstantItems(data);
         content.insertAdjacentHTML("beforeend", items);
         this.addListenersToBetInput();
     },
