bb058d124a50f7b20b19aff3904f6cc0bf887266
AJL72 <agocs.janos.x@gmail.com>
2020-10-14 13:55:14 +0200

    CheckoutController.java applied

---
+++ src/main/java/com/codecool/shop/controller/CheckoutController.java
@@ -0,0 +1,31 @@
+package com.codecool.shop.controller;
+
+import com.codecool.shop.config.TemplateEngineUtil;
+import com.codecool.shop.dao.CountryDao;
+import com.codecool.shop.dao.MatchDetailsDao;
+import com.codecool.shop.dao.SportTypeDao;
+import com.codecool.shop.dao.implementation.CountryDaoMem;
+import com.codecool.shop.dao.implementation.MatchDetailsDaoMem;
+import com.codecool.shop.dao.implementation.SportTypeDaoMem;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = {"/checkout"})
+public class CheckoutController extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
+        WebContext context = new WebContext(req, resp, req.getServletContext());
+
+        resp.setContentType("text/html; charset=UTF-8");
+        engine.process("checkout.html", context, resp.getWriter());
+    }
+}
