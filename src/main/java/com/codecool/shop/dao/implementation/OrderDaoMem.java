2a7761a05ec10d107d3049d7d4947bfb98252821
schonciprian <60438162+schonciprian@users.noreply.github.com>
2020-10-15 11:24:51 +0200

    Refactor

--- src/main/java/com/codecool/shop/dao/implementation/OrderDaoMem.java
+++ src/main/java/com/codecool/shop/dao/implementation/OrderDaoMem.java
@@ -4,25 +4,21 @@ import com.codecool.shop.dao.CartDao;
 import com.codecool.shop.dao.OrderDao;
 import com.codecool.shop.model.Order;
 
+import java.util.List;
+
 public class OrderDaoMem implements OrderDao {
 
-    private String firstName;
-    private String lastName;
-    private int phoneNumber;
-    private String emailAddress;
-    private String country;
-    private String city;
-    private int zip;
-    private String Address;
+    private List<Order> listOfOrders;
+    private static OrderDaoMem instance = null;
 
-    public OrderDaoMem(String firstName, String lastName, String emailAddress, String country, String city, int zip, String address) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.emailAddress = emailAddress;
-        this.country = country;
-        this.city = city;
-        this.zip = zip;
-        Address = address;
+    private OrderDaoMem() {
+    }
+
+    public static OrderDaoMem getInstance() {
+        if (instance == null) {
+            instance = new OrderDaoMem();
+        }
+        return instance;
     }
 
     @Override
--- src/main/java/com/codecool/shop/model/Order.java
+++ src/main/java/com/codecool/shop/model/Order.java
@@ -2,6 +2,26 @@ package com.codecool.shop.model;
 
 public class Order extends BaseModel {
 
+    private String firstName;
+    private String lastName;
+    private int phoneNumber;
+    private String emailAddress;
+    private String country;
+    private String city;
+    private int zip;
+    private String Address;
+
+    public Order(String description, String firstName, String lastName, int phoneNumber, String emailAddress, String country, String city, int zip, String address) {
+        super(description);
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.emailAddress = emailAddress;
+        this.country = country;
+        this.city = city;
+        this.zip = zip;
+        Address = address;
+    }
 
     public Order(String description) {
         super(description);
