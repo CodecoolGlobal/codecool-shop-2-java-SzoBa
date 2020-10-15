4007a4b233ef711c0bf8443426e58893bdbae053
Balázs Szolcsánszki <bszolcsan@gmail.com>
2020-10-15 14:58:12 +0200

    Add order save to file

--- src/main/java/com/codecool/shop/controller/OrderConfirmedController.java
+++ src/main/java/com/codecool/shop/controller/OrderConfirmedController.java
@@ -8,6 +8,8 @@ import com.codecool.shop.dao.implementation.OrderDaoMem;
 import com.codecool.shop.model.Cart;
 import com.codecool.shop.model.CartItem;
 import com.codecool.shop.model.Order;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
 
@@ -19,7 +21,11 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Properties;
 
 @WebServlet(urlPatterns = {"/success"})
@@ -98,7 +104,22 @@ public class OrderConfirmedController extends HttpServlet {
 
             // TODO
             // save order to json/txt
+        Date date = new Date();
+        SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy");
+        String dateString = formatter.format(date);
+        String fileName = order.getId() + dateString;
 
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        String orderString = gson.toJson(order);
+
+        try {
+            File myFile = new File(fileName + ".txt");
+            FileWriter myFileWriter = new FileWriter(fileName + ".txt");
+            myFileWriter.write(orderString);
+            myFileWriter.close();
+        } catch (IOException e) {
+            System.out.println("Problem occurred during writing to file");
+        }
 
             orderDao.remove(clientSessionIdHashCode);
             cartDao.remove(clientSessionIdHashCode);
