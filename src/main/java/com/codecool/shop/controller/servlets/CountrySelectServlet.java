ab77ecb36fec0655cc5b496ef18514d54f284f26
schonciprian <60438162+schonciprian@users.noreply.github.com>
2020-10-15 10:19:44 +0200

    Modified to get all search results

--- src/main/java/com/codecool/shop/controller/servlets/CountrySelectServlet.java
+++ src/main/java/com/codecool/shop/controller/servlets/CountrySelectServlet.java
@@ -2,10 +2,13 @@ package com.codecool.shop.controller.servlets;
 
 import com.codecool.shop.dao.CountryDao;
 import com.codecool.shop.dao.MatchDetailsDao;
+import com.codecool.shop.dao.SportTypeDao;
 import com.codecool.shop.dao.implementation.CountryDaoMem;
 import com.codecool.shop.dao.implementation.MatchDetailsDaoMem;
+import com.codecool.shop.dao.implementation.SportTypeDaoMem;
 import com.codecool.shop.model.Country;
 import com.codecool.shop.model.MatchDetails;
+import com.codecool.shop.model.SportType;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
@@ -22,24 +25,33 @@ public class CountrySelectServlet extends javax.servlet.http.HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        int typeId = Integer.parseInt(req.getParameter("typeId"));
         int countryId = Integer.parseInt(req.getParameter("countryId"));
+
+        SportTypeDao sportTypeDataStore = SportTypeDaoMem.getInstance();
+        SportType selectedType = sportTypeDataStore.find(typeId);
+
         CountryDao countryDataStore = CountryDaoMem.getInstance();
         Country selectedCountry = countryDataStore.find(countryId);
 
         MatchDetailsDao matchDetailsDataStore = MatchDetailsDaoMem.getInstance();
-        List<MatchDetails> matchesBySelectedCountry;
+        List<MatchDetails> matchesSelected;
 
-        if (countryId == 0) {
-            matchesBySelectedCountry = matchDetailsDataStore.getAll();
+        if (countryId == 0 && typeId == 0) {
+            matchesSelected = matchDetailsDataStore.getAll();
+        } else if (typeId == 0) {
+            matchesSelected = matchDetailsDataStore.getBy(selectedCountry);
+        } else if (countryId == 0) {
+            matchesSelected = matchDetailsDataStore.getBy(selectedType);
         } else {
-            matchesBySelectedCountry = matchDetailsDataStore.getBy(selectedCountry);
+            matchesSelected = matchDetailsDataStore.getBy(selectedType, selectedCountry);
         }
 
         Gson gson = new GsonBuilder()
                 .excludeFieldsWithoutExposeAnnotation()
                 .create();
 
-        String matches = gson.toJson(matchesBySelectedCountry);
+        String matches = gson.toJson(matchesSelected);
 //        resp.setContentType("application/json");
         resp.setCharacterEncoding("UTF-8");
         PrintWriter out = resp.getWriter();
