2021fdc43a570dca844b99353383fcd4e19ec3d1
schonciprian <60438162+schonciprian@users.noreply.github.com>
2020-10-28 14:49:12 +0100

    Added test class to test SportTypeDaoJdbc

---
+++ src/test/java/com/codecool/shop/dao/jdbc_implementation/SportTypeDaoJdbcTest.java
@@ -0,0 +1,33 @@
+package com.codecool.shop.dao.jdbc_implementation;
+
+import com.codecool.shop.dao.implementation.SportTypeDaoMem;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SportTypeDaoJdbcTest {
+
+    SportTypeDaoJdbc testSportTypeDaoJDBC;
+
+    @BeforeEach
+    void setup() {
+        
+    }
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void find() {
+    }
+
+    @Test
+    void remove() {
+    }
+
+    @Test
+    void getAll() {
+    }
+}
