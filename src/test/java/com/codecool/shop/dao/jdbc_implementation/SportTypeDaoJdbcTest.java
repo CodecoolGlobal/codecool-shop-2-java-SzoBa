436b93d15995d120cb253c76e123444786eefcf2
schonciprian <60438162+schonciprian@users.noreply.github.com>
2020-10-28 15:28:46 +0100

    Implemented setup method for testing

--- src/test/java/com/codecool/shop/dao/jdbc_implementation/SportTypeDaoJdbcTest.java
+++ src/test/java/com/codecool/shop/dao/jdbc_implementation/SportTypeDaoJdbcTest.java
@@ -1,18 +1,48 @@
 package com.codecool.shop.dao.jdbc_implementation;
 
 import com.codecool.shop.dao.implementation.SportTypeDaoMem;
+import com.codecool.shop.model.SportType;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+
+
+import javax.sql.DataSource;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 class SportTypeDaoJdbcTest {
 
-    SportTypeDaoJdbc testSportTypeDaoJDBC;
+    @Mock
+    private DataSource ds;
 
-    @BeforeEach
-    void setup() {
-        
+    @Mock
+    private Connection c;
+
+    @Mock
+    private PreparedStatement stmt;
+
+    @Mock
+    private ResultSet rs;
+
+    private SportType sportType;
+
+    @BeforeAll
+    void setup() throws SQLException {
+        assertNotNull(ds);
+        when(c.prepareStatement(any(String.class))).thenReturn(stmt);
+        when(ds.getConnection()).thenReturn(c);
+
+        sportType = new SportType("Football", "Football description");
+
     }
 
     @Test
